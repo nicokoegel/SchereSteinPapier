909f4dd6182d6e79ef4509ea6f597c70c8ecad5c
nicokoegel <koegel99@gmail.com>
2020-05-11 09:54:36 +0200

    Updates

---
+++ precache-manifest.6c22041360801ce797b7ff7695f3f25d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb678a7c5167cfc50e06db3e9ba937c0",
+    "url": "/SchereSteinPapier/index.html"
+  },
+  {
+    "revision": "93a3ba9551743a5d2e8c",
+    "url": "/SchereSteinPapier/static/css/main.a6d1b3c4.chunk.css"
+  },
+  {
+    "revision": "7c62645b8db6ba27d568",
+    "url": "/SchereSteinPapier/static/js/2.0b53951d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/SchereSteinPapier/static/js/2.0b53951d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93a3ba9551743a5d2e8c",
+    "url": "/SchereSteinPapier/static/js/main.65284922.chunk.js"
+  },
+  {
+    "revision": "c626c9549e9962f7c0d0",
+    "url": "/SchereSteinPapier/static/js/runtime-main.3dac443d.js"
+  },
+  {
+    "revision": "9ac572eb059f9cae179e692e3fe5700b",
+    "url": "/SchereSteinPapier/static/media/versus.9ac572eb.png"
+  }
+]);
